b2a5c0ac8a67dd92f8bb82ff8c031fd03c043d31
melwhiteastonia <56660196+melwhiteastonia@users.noreply.github.com>
2019-11-28 15:17:22 -0500

    Add files via upload

--- project3.s
+++ project3.s
@@ -1,8 +1,8 @@
 
 input: .space 1032
-newline: .asciiz "\n"
+newline: .asciiz "\n" #prints it on a new line
 error: .asciiz "NaN" #prints this for invalid outputs
-
+comma: .ascizz "," #prints comma
 
 main : 
 
@@ -10,4 +10,15 @@ li $v0, 8 #syscall for user input
 la $a0, input #stores the input in the argument register
 li $a1, 1001  # maximum input of 1000 charactars
 
+jal SubA
+
 move $s0, $a0 #moves the input to a temp register. 
+
+SubA:
+
+lb $t0, ($a0)
+
+
+charactars: 
+
+beq 
